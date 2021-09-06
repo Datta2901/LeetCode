c9a7443be720fd4fe5b3411324f42642be36db5a
manikanta2901 <manikanta2901@gmail.com>
2021-09-06 18:04:34 +0530

    Array template added

---
+++ ArrayTemplate.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+};
+
+
+
+int main(){
+    Solution obj;
+    vector<int>num;
+    int size;
+    cin >> size;
+    for(int i = 0; i < size; i++){
+        int a;
+        cin >> a;
+        num.push_back(a);
+    }
+    return 0;
+}
