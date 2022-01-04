baa7b666760e646dd056eb222cb4cfaf9b81212c
manikanta2901 <manikanta2901@gmail.com>
2022-01-04 19:08:01 +0530

    Reshale the Matrix added

--- TopicWise/Arrays/Easy/566. Reshape the Matrix.cpp
+++ TopicWise/Arrays/Easy/566. Reshape the Matrix.cpp
@@ -3,31 +3,30 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-     int n = mat.size();
-     int m = mat[0].size();
+    vector<vector<int> > matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int n = mat.size();
+        int m = mat[0].size();
         if(r * c != n * m){
             return mat;
         }
-      vector<vector<int> > res(r, vector<int>(c));
+        vector<vector<int> > res(r, vector<int>(c));
         vector<int>temp;
-      for (int i = 0; i < n; i++) {
-         for (int j = 0; j < m; j++) {
-            temp.push_back(mat[i][j]);
-         }
-      }
-      int count = 0;
-      for (int i = 0; i < r; i++) {
-         for (int j = 0; j < c; j++) {
-            res[i][j] = temp[count++];
-         }
-      }
-      return res;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                temp.push_back(mat[i][j]);
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                res[i][j] = temp[count++];
+            }
+        }
+        return res;
     }
 };
 
 int main(){
 
     return 0;
-
 }
