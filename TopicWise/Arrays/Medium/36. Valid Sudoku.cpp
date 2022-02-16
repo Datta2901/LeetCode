09699ecabf367024465688a51bf8c9905ce840c2
manikanta2901 <manikanta2901@gmail.com>
2022-02-16 14:16:40 +0530

    sudoku solution updated

--- TopicWise/Arrays/Medium/36. Valid Sudoku.cpp
+++ TopicWise/Arrays/Medium/36. Valid Sudoku.cpp
@@ -27,30 +27,6 @@ public:
     }
 };
 
-
-// class Solution {
-// public:
-//     bool isValidSudoku(vector<vector<char> >& board){
-//         int m = board.size();
-//         int n = board[0].size();
-//         unordered_set<char>row[m],column[n],subArray[m / 3][n / 3];
-//         for(int i = 0; i < m; i++){
-//             for(int j = 0; j < n; j++){
-//                 char a = board[i][j];
-//                 if(a != '.'){
-//                     if(row[i].count(a) || column[j].count(a) || subArray[i / 3][j / 3].count(a)){
-//                         return false;
-//                     }
-//                     row[i].insert(a);
-//                     column[j].insert(a);
-//                     subArray[i / 3][j / 3].insert(a);
-//                 }
-//             }
-//         }
-//         return true;
-//     }
-// };
-
 // Same idea 
 class Solution {
 public:
