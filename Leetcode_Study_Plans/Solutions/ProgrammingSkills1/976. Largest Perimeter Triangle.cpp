983aad8ad8e468a5dc51c15a35e7b869a084f1e9
manikanta2901 <manikanta2901@gmail.com>
2022-03-29 19:07:38 +0530

    qp 5

---
+++ Leetcode_Study_Plans/Solutions/ProgrammingSkills1/976. Largest Perimeter Triangle.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end(),greater<int>());
+        for(int i = 0; i <= n - 2; i++){
+            if(nums[i] < nums[i + 1] + nums[i + 2]){
+                return nums[i] + nums[i + 1] + nums[i + 2];
+            }
+        }
+        return 0;
+    }
+};
+
+
+
+int main(){
+    Solution obj;
+    vector<int>num;
+    int size;
+    cin >> size;
+    for(int i = 0; i < size; i++){
+        int a;
+        cin >> a;
+        num.push_back(a);
+    }
+    cout << obj.largestPerimeter(num) << endl;
+    return 0;
+}
