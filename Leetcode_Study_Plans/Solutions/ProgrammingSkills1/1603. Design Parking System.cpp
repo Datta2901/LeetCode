87c204ec7cccdbbdeb0c700171edad4273748e9c
manikanta2901 <manikanta2901@gmail.com>
2022-04-10 13:02:02 +0530

    Added 18th Solution

---
+++ Leetcode_Study_Plans/Solutions/ProgrammingSkills1/1603. Design Parking System.cpp
@@ -0,0 +1,40 @@
+class ParkingSystem {
+public:
+    int b,m,s;
+    ParkingSystem(int big, int medium, int small) {
+        b = big;
+        m = medium;
+        s = small;
+    }
+    
+    bool addCar(int carType) {
+        if(carType == 1){
+            if(b >= 1){
+                b--;
+                return true;
+            }
+            
+        }
+        if(carType == 2){
+            if(m >= 1){
+                m--;
+                return true;
+            }
+            
+        }
+        if(carType == 3){
+            if(s >= 1){
+                s--;
+                return true;
+            }
+            
+        }
+        return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
